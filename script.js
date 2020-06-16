86492504d9438d80877b7e90e28bb94b0b7c3a1d
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-06-16 13:39:42 +0200

    Add song results to DOM and style it

--- script.js
+++ script.js
@@ -5,25 +5,34 @@ const nextReultsContainer = document.getElementById('more-results');
 
 let searchValue = "";
 
-
 // Update DOM with search results
 const updateResults = (data) => {
     const output = document.createElement('ul');
-
+    output.classList.add('songs');
     data.data.forEach(item => {
         const result = document.createElement('li');
         result.innerHTML = `
         <li>
-        <span><strong>${item.artist.name}</strong>${item.title}</span>
-        <button>Get Lyrics</button>
+        <span><strong>${item.artist.name}</strong> - ${item.title}</span>
+        <button class="btn lyrics-btn">Get Lyrics</button>
         </li>
         `
 
         output.appendChild(result);
     });
 
+    resultsContainer.innerHTML = "";
     resultsContainer.appendChild(output);
 
+    // Check if there more than 15 songs
+    if (data.next || data.prev) {
+        nextReultsContainer.innerHTML = `
+        ${data.prev ? `<button class='btn change-btn'>Prev</button>` : ''}
+        ${data.next ? `<button class='btn change-btn'>Next</button>` : ''}
+        `
+    } else {
+        nextReultsContainer.innerHTML = '';
+    }
 }
 
 // Search song
